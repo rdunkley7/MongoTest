41465e7ecaa9b373ee549b8bee01eeebb76aabdd
Rachel Dunkley <rdunks7@gmail.com>
2019-03-31 13:20:50 -0500

    Added update document, and delete document, and printing method.

--- src/main/java/MongoTest.java
+++ src/main/java/MongoTest.java
@@ -21,12 +21,12 @@ import java.util.List;
 
 public class MongoTest {
 
-   public static void main(String[] args){
+   public static void main(String[] args) {
        System.out.print("Hello World");
 
        //For atlas connection instead of using local host
-      // MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
-      // MongoClient mongoClient = new MongoClient(connectionString);
+       // MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+       // MongoClient mongoClient = new MongoClient(connectionString);
 
 
        MongoClient mongoClient = new MongoClient();
@@ -34,14 +34,51 @@ public class MongoTest {
        MongoCollection<Document> collection = database.getCollection("Songs");
 
 
+       //print the docs
+       printAll();
+
        Document doc = new Document("Artist", "Eagles")
                .append("Song", "Hotel California")
                .append("Year", "1976");
 
 
+       // insert document
        collection.insertOne(doc);
 
+       //print the docs
+       printAll();
+
+
+       // update value
+       collection.updateOne(eq("Artist", "Eagles"), new Document("$set", new Document("Artist", "The Eagles")));
+
+       //print the docs
+       printAll();
+
+
+       collection.deleteOne(eq("Artist", "The Eagles"));
+
+       //print the docs
+       printAll();
 
    }
 
+    public static void printAll(){
+       MongoClient mongoClient = new MongoClient();
+       MongoDatabase database = mongoClient.getDatabase("Music");
+       MongoCollection<Document> collection = database.getCollection("Songs");
+       MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
+
 }
+
+
+
